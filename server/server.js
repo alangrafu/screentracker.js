948037d0985634d6e679334010751553f8b497d0
alangrafu <alvaro@graves.cl>
2013-12-20 15:39:15 -0300

    saves data in file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 9999});
+  , wss = new WebSocketServer({port: 9999})
+  , fs = require('fs');
+
+var filename = "stream_"+((new Date()).getTime())+"_"+(Math.floor(10000000* Math.random()));
+var stream = fs.createWriteStream(filename);
+
+process.stdout.write("Data will be stored in "+filename+"\n");
+process.stdout.write("Terminate this process when you have obtained enough data.\n\n");
+process.on('exit', function (){
+  process.stdout.write("Goodbye!\n");
+  stream.end();
+});
+
 wss.on('connection', function(ws) {
     ws.on('message', function(message) {
-        console.log(JSON.parse(message));
+        stream.write(JSON.stringify(JSON.parse(message))+"\n");
     });
-    ws.send('something');
 });
